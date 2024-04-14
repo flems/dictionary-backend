53fb1fe105b7979ed3500a1730cc686e4f54dd0d
Serg <borisov.s3@yandex.ru>
2024-04-14 20:30:31 +0300

    upd

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api')
   app.use(cookieParser())
   app.enableCors({
-    origin: process.env.ORIGIN.split(',') || ['http://localhost:3000'],
+    origin: process.env.ORIGIN.split(','),
 		credentials: true,
 		exposedHeaders: 'set-cookie'
   })
