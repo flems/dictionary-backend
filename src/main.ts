5a7534ddc7b23a316f46d7c6e5a4bee9c06743d7
Serg <borisov.s3@yandex.ru>
2024-04-19 22:50:42 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ async function bootstrap() {
   dotenv.config()
   const app = await NestFactory.create(AppModule)
 
+  // console.log(process.env.ORIGIN.split(','));
+  
   
   app.setGlobalPrefix('api')
   app.use(cookieParser())
   app.enableCors({
-    origin: process.env.ORIGIN.split(','),
+    origin: ['http://localhost:3000', 'http://31.129.45.72'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
   })
