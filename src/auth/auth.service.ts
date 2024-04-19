4e334d6eaf76f345fa47082b78d8975a5a9bcbda
Serg <borisov.s3@yandex.ru>
2024-04-19 22:58:37 +0300

    fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -90,7 +90,7 @@ export class AuthService {
 
         res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
             httpOnly: true,
-            domain: process.env.DOMAIN || 'localhost',
+            domain: 'localhost',
             expires: expiresIn,
             // if producation, для https
             secure: true,
@@ -102,7 +102,7 @@ export class AuthService {
     removeRefreshTokenToResponse(res: Response) {
         res.cookie(this.REFRESH_TOKEN_NAME, '', {
             httpOnly: true,
-            domain: process.env.DOMAIN || 'localhost',
+            domain: 'localhost',
             expires: new Date(0),
             // if producation, для https
             secure: true,
